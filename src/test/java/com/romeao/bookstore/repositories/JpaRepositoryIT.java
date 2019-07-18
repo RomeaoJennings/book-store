75cbcaeadc7efd185accef95f089db917141cbf5
Romeao Jennings <romeao.jennings@gmail.com>
2019-07-17 23:15:54 -0700

    Refactor JPA tests into single file to speed up integration tests

--- src/test/java/com/romeao/bookstore/repositories/JpaRepositoryIT.java
+++ src/test/java/com/romeao/bookstore/repositories/JpaRepositoryIT.java
@@ -1,6 +1,7 @@
 package com.romeao.bookstore.repositories;
 
 import com.romeao.bookstore.domain.Author;
+import com.romeao.bookstore.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,20 +10,36 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
-class AuthorRepositoryIT {
+class JpaRepositoryIT {
+    @Autowired
+    BookRepository bookRepository;
     @Autowired
     private AuthorRepository authorRepository;
 
     @Test
     void beansAreNotNull() {
         assertNotNull(authorRepository);
+        assertNotNull(bookRepository);
     }
 
     @Test
-    void testRepositoryLoadsRelatedEntities() {
+    void testAuthorRepositoryLoadsRelatedEntities() {
+        // when
         Author author = authorRepository.findById(1).orElse(null);
 
+        // then
         assertNotNull(author);
         assertNotEquals(0, author.getBooks().size());
+    }
+
+    @Test
+    void testBookRepositoryLoadsRelatedEntities() {
+        // when
+        Book book = bookRepository.findById(1).orElse(null);
+
+        // then
+        assertNotNull(book);
+        assertNotEquals(0, book.getAuthors().size());
+        assertNotEquals(0, book.getGenres().size());
     }
 }
